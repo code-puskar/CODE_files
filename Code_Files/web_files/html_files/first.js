042268212b17acfa5b0cf5d5899974beba1faae8
Puskar Debnath <puskardebnath14@gmail.com>
2024-04-17 10:57:01 +0530

    commit

---
+++ Code_Files/web_files/html_files/first.js
@@ -0,0 +1,54 @@
+
+console.log("DOD");
+// var a = [12,13];  //array in js []
+// var b=a;
+// b.pop();
+
+//conditionals if else-else if ----------------->
+//jab bhi baat agar magar pe ayegi aur aisa waisa pe ayi toh tub use hote hai conditionals
+// if (12>13) {
+    
+// }
+// else{
+
+// }
+
+// LOOPS------------------------------>
+// for and while 
+// for(var i=0;i<11;i++){
+// console.log(i);
+
+// }
+
+//WHILE LOOPS
+// var a = 10;
+// while (a<20) {
+//     console.log(a);
+// a++;
+    
+// }
+
+// FUNCTIONS:
+//1.jub apka code app future mein chalana vhahte ho aur reuse karna hai ya phir wanna use with diiferent data
+//koi code ko likh kar naam dena
+
+// function kill(a){
+//     console.log(a);
+    
+// }
+// kill(12);
+/*function kill(a,b,c){//Parameters:jaha store hoti hai value------------------------------->functions
+    console.log(a,b,c);
+    
+}
+kill(12,13,14);*///argurment-real values during chalate waqt
+
+//push pop shift unshift: arr.push(x)----->add element arr.unshift(x)----->add at start arr.shift(x)----->remove from start
+//arr.splice(2,1): matlab 2nd position se 1 element gayab
+
+
+
+//ARRAY-Group of values.
+
+var arr=[1,2,3,4,5];
+arr[0];

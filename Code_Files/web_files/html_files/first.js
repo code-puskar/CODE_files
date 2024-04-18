7356ba7476aa203d2be5dc122d9ac5bbd3fe1a11
Puskar Debnath <puskardebnath14@gmail.com>
2024-04-18 23:36:46 +0530

    commit

--- Code_Files/web_files/html_files/first.js
+++ Code_Files/web_files/html_files/first.js
@@ -72,10 +72,19 @@ ACCESS---> car.age= 24;
 ADVANCE JAVA SCRIPT
  1. var function scopedd hota hai: u can use  variable any where in a function, eg; for loop mein declared i ko loop k bahar bhi use kar sakte hai 
  2. let braces scoped hota hai: u  can use only inside the declared bracket.eg; loop k bahar i nahi chalega.
- 3. var adds itself to the window objects and let doesnt adds 
-
-
-
+ 3. var adds itself to the window objects and let doesnt adds - Windows have its own features collection or local IN window
+ 4. BROWSER context API- windows , stack and Heap Memory.
+ 5. STACK:jaise aate waise jaate 
+ 6.HEAP: var data is stored in heap.
+ 7.EXECUTION CONTEXT: any time w use  function, function will create its 3 imaginary container 
+ i}variables.
+ ii}functions inside that parent fnc.
+ iii} lexical environment of that function.
+8. copy reference:
+var a=[1,2,3];
+var a = {name:"hiru"};
+var x= [...a] ...=iske baadh jobhi likha hai uske value spread operators k jagah rakh do
+9.TRUTHY AND FALSY: 
 
 
 */

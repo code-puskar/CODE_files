bfeb34e731b8875f4bdf01a37e1a203ccca2e1b5
Puskar Debnath <puskardebnath14@gmail.com>
2024-04-27 16:40:32 +0530

    commit

--- Code_Files/C_files/insertion_sort.c
+++ Code_Files/C_files/insertion_sort.c
@@ -1,5 +1,22 @@
 #include <stdio.h>
 int main(){
     
+int size =5;
+int arr[]={5,3,2,4,1};
+for (int i = 1; i < size; i++)
+{
+    for (int j = i-1; j>0; j--)
+    {
+       
+        
+    }
+    
+}
+for (int i = 0; i < size; i++)
+{
+  printf("%d",arr[i]);  /* code */
+}
+
+    
 return 0;
 }

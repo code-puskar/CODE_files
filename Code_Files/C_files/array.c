008175a544d70867f047dc51f594c8bdbd454574
Puskar Debnath <puskardebnath14@gmail.com>
2024-05-13 18:11:36 +0530

    commit

--- Code_Files/C_files/array.c
+++ Code_Files/C_files/array.c
@@ -1,20 +1,43 @@
 #include <stdio.h>
+
+int binarysearch(int arr[],int size,int element){
+int start=0;
+int end=size-1; 
+while (start<=end)
+ 
+{int mid = start + (end - start) / 2;
+  if (arr[mid]==element)
+  {
+    return mid;
+
+  }
+  else if (element<=arr[mid])
+  {
+    start=0;
+    end=mid-1;
+  }
+  else
+  {
+    start=mid+1;
+  }
+  
+  
+  
+}
+return -1;
+}
+
 int main(){
-    int n;
-    printf("enter the length of array: ");
-    scanf("%d",&n);
-    int arr[n],i,j;
-    for ( i = 1; i <=n; i++)
-    {
-        printf("\nenter element %d of array: ",i);
-        scanf("%d",&arr[i]);
-    }
-    printf("reversed array is:");
-    for ( j = n; j >=1; j--)
-    {
-        printf("%d",arr[j]);
-    }
+    int size= 10;
+    int arr[]={2,3,4,5,8,10,20,24,29,35};
+    int a=binarysearch(arr,size,20);
+    printf("%d,:position of your element",a);    
+    
     
 
 return 0;
 }
+
+    
+
+

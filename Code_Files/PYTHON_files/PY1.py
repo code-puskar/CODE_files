90c642a57ca40e2b578f5d62d56bcfa20574f864
Puskar Debnath <puskardebnath14@gmail.com>
2024-11-29 10:05:45 +0530

    Merge branch 'main' of https://github.com/code-puskar/CODE_files

---
+++ Code_Files/PYTHON_files/PY1.py
@@ -0,0 +1,15 @@
+
+# name = input("enter your name: ")
+# roll_no =int(input("enter your roll_no"))
+# print("print the information ",name,roll_no)
+# print("*"*20)
+# print("calculator")
+# a=10
+# b=20
+# c=a+b
+# print(f"{a} + {b} = {c}")
+if 12321<2389:
+    print("valid")
+
+else:
+    print("not sanctioned")

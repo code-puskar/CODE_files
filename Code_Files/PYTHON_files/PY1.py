827f64c7f6a6949a07f525b88d37b38dc6c0f3df
Puskar Debnath <puskardebnath14@gmail.com>
2024-12-03 13:34:17 +0530

    commit

--- Code_Files/PYTHON_files/PY1.py
+++ Code_Files/PYTHON_files/PY1.py
@@ -1,17 +1,22 @@
+# a= 100
+# print("the type of variable having value",a,"is", type(a))
+# b= 100
+# print("the type of variable having value",b,"is", type(b))
+# c= 100
+# print("the type of variable having value",c,"is", type(c))
 
-# name = input("enter your name: ")
-# roll_no =int(input("enter your roll_no"))
-# print("print the information ",name,roll_no)
-# print("*"*20)
-# print("calculator")
-# a=10
-# b=20
-# c=a+b
-# print(f"{a} + {b} = {c}")
-if 12321<2389:
-    print("valid")
+# my_list =[10,'hello',3.14,[1,2,3]]
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[3][1])
+# print(my_list[1:3])
+# new_list =my_list +[5,6]
+# print(new_list)
 
+
+a=input("enter Username: ")
+b=input("enter Password: ")
+if a=="name" and b=="password":
+    print("login successful:)")
 else:
-    print("not sanctioned")
-
-    
+    print("try again!")

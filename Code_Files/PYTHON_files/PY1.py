f2897112377105e9772c9df1910d25e27a272b24
Puskar Debnath <puskardebnath14@gmail.com>
2024-12-17 22:08:29 +0530

    commit

--- Code_Files/PYTHON_files/PY1.py
+++ Code_Files/PYTHON_files/PY1.py
@@ -1,11 +1,11 @@
-# a= 100
+# a= 100                                                          <--------TYPE DEFINING
 # print("the type of variable having value",a,"is", type(a))
 # b= 100
 # print("the type of variable having value",b,"is", type(b))
 # c= 100
 # print("the type of variable having value",c,"is", type(c))
 
-# my_list =[10,'hello',3.14,[1,2,3]]
+# my_list =[10,'hello',3.14,[1,2,3]]                                <---------LIST 
 # print(my_list[0])
 # print(my_list[1])
 # print(my_list[3][1])
@@ -14,9 +14,19 @@
 # print(new_list)
 
 
-a=input("enter Username: ")
-b=input("enter Password: ")
-if a=="name" and b=="password":
-    print("login successful:)")
-else:
-    print("try again!")
+# a=input("enter Username: ")                                        <---------CONDITIONS
+# b=input("enter Password: ")
+# if a=="name" and b=="password":
+#     print("login successful:)")
+# else:
+#     print("try again!")
+
+# def demoFunc(x,y=5):
+#     print(f"x:{x}")
+#     print(f"y:{y}")
+# demoFunc(3)
+
+s="Python Programmming"
+print(s.lower())
+print(s.upper())
+print(s.swapcase())
